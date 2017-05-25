c871954ffac42308ee0fe8b0351596ba1aa6721e
nekuneko <noct@Melchior.local>
2017-05-25 02:06:48 +0200

    funcion usaDropbox para el futuro, levelup y atacar
    la funcion levelup cuando se creaba un pokemon hacia que este no se
    curase

--- pokemon_db.py
+++ pokemon_db.py
@@ -3,6 +3,12 @@ from pokemon_t import *
 from movimientos_db import *
 import json
 
+# Antes de usar la clase Pokémon se tiene que llamar a esta función
+def usaDropbox (param = False):
+	global bool_usaDropbox
+	bool_usaDropbox = param
+
+
 
 pokemon_legendarios = {'moltres', 'zapdos', 'articuno', 'mewtwo', 'mew', 'raikou', 'entei',
 											 'suicune', 'lugia', 'ho-oh', 'celebi', 'regirock', 'regice', 'registeel',
@@ -10,12 +16,42 @@ pokemon_legendarios = {'moltres', 'zapdos', 'articuno', 'mewtwo', 'mew', 'raikou
 											 'uxie', 'mesprit', 'azelf', 'dialga', 'palkia', 'heatran', 'regigigas',
 											 'giratina', 'cresselia', 'phione', 'manaphy', 'darkrai', 'shaymin', 'arceus'}
 
-pokemon_db = { 0: 'MissingNo.', 1: 'Bulbasaur', 382: 'Kyogre', 383: 'Groudon'}
-pokemon_db_t = {0: NORMAL, 1: PLANTA, 382: AGUA, 383: TIERRA}
+# Si el pokémon está en la base de datos significa que tenemos:
+# número, nombre, statsBase, foto delantera, foto trasera, y sonido
+pokemon_db = {
+	0: {'nombre': "MissingNo.", 
+			'tipo': NORMAL,
+	 		'stats': {
+	 	 		'ps': 120, 'ataque': 120, 'defensa': 120, 
+	 	 		'ataque_esp': 120, 'defensa_esp': 120, 'velocidad': 120}},
+ 	1: {'nombre': "Bulbasaur", 
+			'tipo': PLANTA,
+	 		'stats': {
+	 	 		'ps': 45, 'ataque': 49, 'defensa': 49, 
+	 	 		'ataque_esp': 65, 'defensa_esp': 65, 'velocidad': 45}},
+ 	382: {'nombre': "Kyogre",
+ 				'tipo': AGUA,
+ 				'stats': {
+	 	 			'ps': 100, 'ataque': 100, 'defensa': 90, 
+	 	 			'ataque_esp': 150, 'defensa_esp': 140, 'velocidad': 90}},						 
+ 	383: {'nombre': "Groudon",
+ 				'tipo': TIERRA,
+ 				'stats': {
+	 	 			'ps': 100, 'ataque': 150, 'defensa': 140, 
+	 	 			'ataque_esp': 100, 'defensa_esp': 90, 'velocidad': 90}}}
 
 
+def cargarBaseDatosPkm ():
+	with open("pokemon.db") as json_file:
+		pokemon_db = json_file
 
-def formatearPokemon (pkm):
+def guardarBaseDatosPkm ():
+	with open("pokemon.db", 'w') as outfile:
+		json.dump(pokemon_db, outfile)
+
+
+def formatearPokemon (set_pkm):
+	pkm = set_pkm
 	pkm['numero'] 			= int(pkm['numero'])
 	pkm['nombre'] 			= str(pkm['nombre']).title()
 	pkm['tipo']					= int(dic_tipo_inv[pkm['tipo'].lower()])
@@ -68,26 +104,40 @@ def setToPokemon (set_p):
 
 
 
+
 class Pokemon:
 
-	# Pokémon dado el nombre, nivel y estado salvaje 
+	# Pokémon dado el nombre, nivel y a quien pertenece, si 0 es salvaje
 	def __init__ (self, int_numero = 0, int_nivel = 5, int_id_entrenador = 0):
-		
-		# Buscar pokemon por número INCOMPLETO
-		if (int_numero in pokemon_db):
-			self.nombre = pokemon_db[int_numero]
-			self.tipo = pokemon_db_t[int_numero]
-		else: # Buscar el tipo del pokemon en database IMCOMPLETO
-			self.nombre = "INCOMPLETO"
-			self.tipo = NOTIPO
 
 		# Establacer número de pokémon
-		self.numero  = int_numero
+		self.numero = int_numero
 
 		# Establecer Nivel 
 		if (int_nivel < 5):
 			int_nivel = 5
 		self.nivel = int_nivel
+
+		# Buscar pokemon por número en la base de datos local, si está en la base de datos
+		# local tenemos las imágenes y todo
+		if (int_numero in pokemon_db):
+			self.nombre = pokemon_db[int_numero]['nombre']
+			self.tipo = pokemon_db[int_numero]['tipo']
+		else:
+			try:
+				if (bool_usaDropbox == True):
+					pass # INCOMPLETO
+				else: # Pokémon MissingNo.
+					self.nombre = pokemon_db[0]['nombre']
+					self.tipo = pokemon_db[0]['tipo']
+					self.numero = 0
+
+			# Esta escepción salta cuando no se ha establecido que se use el dropbox ó
+			# ha fallado la búsqueda
+			except Exception as e:
+				self.nombre = pokemon_db[0]['nombre']
+				self.tipo = pokemon_db[0]['tipo']
+				self.numero = 0
 
 		# Establecer ID de entrenador
 		self.id_entrenador = int_id_entrenador
@@ -105,19 +155,18 @@ class Pokemon:
 			self.sexo = random.randint(0, 2)
 
 		# Stats base por defecto, genérico
-		self.ps_max 		 = 45
-		self.ps 				 = self.ps_max
-		self.ataque			 = 49
-		self.defensa 		 = 65
-		self.ataque_esp  = 49
-		self.defensa_esp = 65
-		self.velocidad 	 = 45
+		self.ps_max 		 = pokemon_db[self.numero]['stats']['ps']
+		self.ataque			 = pokemon_db[self.numero]['stats']['ataque']
+		self.defensa 		 = pokemon_db[self.numero]['stats']['defensa']
+		self.ataque_esp  = pokemon_db[self.numero]['stats']['ataque_esp']
+		self.defensa_esp = pokemon_db[self.numero]['stats']['defensa_esp']
+		self.velocidad 	 = pokemon_db[self.numero]['stats']['velocidad']
 
 		# actualizar stats base según nivel
 		for i in range(5, int_nivel):
-			self.levelUp()
+		 	self.levelUp()
 
-
+		self.ps 				 = self.ps_max
 
 
 	def setStatsBase (self, ps, ataque, defensa, ataque_esp, defensa_esp, velocidad):
@@ -160,51 +209,81 @@ class Pokemon:
 
 
 	def levelUp (self):
-		if (self.nivel < 100):
+		if (self.nivel >= 100):
+			self.nivel = 100
+		else:
 			self.nivel += 1
-			# actualizar stats, todo sube un 1
-			self.ps_max 			+= 1
-			self.ataque 			+= 1
-			self.ataque_esp  	+= 1
-			self.defensa 			+= 1
-			self.defensa_esp 	+= 1
-			self.velocidad 		+= 1
+
+		# actualizar stats, todo sube un 1
+		self.ps_max 			+= 1
+		self.ataque 			+= 1
+		self.ataque_esp  	+= 1
+		self.defensa 			+= 1
+		self.defensa_esp 	+= 1
+		self.velocidad 		+= 1
 
 
 	def atacar (self, Pokemon_rival, int_sel):
-		ataques_con_pp = [0, 1, 2, 3]
-
-		# Comprobar ataques sin pp
-		for i in range(0, 4):
-			if (self.movimientos[i].pp == 0):
-				ataques_con_pp.remove(i)
-
 		# Si ningún ataque del pokémon tiene PP, este usará combate
-		if (len(ataques_con_pp) == 0): 
+		if (int_sel not in [0, 1, 2, 3]): 
 			mov = movimientos_db['combate']
 		else:
 			mov = self.movimientos[int_sel] # movimiento seleccionado
 
 		
-		if (self.movimientos[int_sel].nombre != '-'): # si no es ataque nulo
-			# b ni idea
-			# e efectividad
-			# v ni idea
+		# Si el movimiento tiene PP suficientes, ataca
+		if (mov.pp > 0):
+			# CÁLCULO DE DAÑO
+			#
+			# DAÑO = 0.01 * B * E * V * ((((0.2 * N + 1) * A * P)/ (25 * D)) + 2) 
+			#
+			# N = Nivel del Pokémon que ataca. 
+			# A = Cantidad de ataque o ataque especial del Pokémon. Si el ataque que utiliza 
+			# 		el Pokémon es físico se toma la cantidad de ataque y si es especial se toma 
+			#  		la cantidad de ataque especial. 
+			# P = Poder del ataque, el potencial del ataque. 
+			# D = Cantidad de defensa del Pokémon rival. Si el ataque que hemos utilizado es físico
+			#  		cogeremos la cantidad de defensa del Pokémon rival, si el ataque que hemos
+			# 		utilizado es especial, se coge la cantidad de defensa especial del Pokémon rival. 
+			# B = Bonificación. Si el ataque es del mismo tipo que el Pokémon que lo lanza toma un 
+			#  		valor de 1.5, si el ataque es de un tipo diferente al del Pokémon que lo lanza 
+			# 		toma un valor de 1. 
+			# E = Efectividad. Puede tomar los valores de 0, 0.25, 0.5, 1, 2 y 4. 
+			# V = Variación. Es una variable que comprende todos los valores discretos entre 85 y 100 (ambos incluidos). 
+				
+			N = self.nivel
+			A = 0
+			P = mov.potencia
+			D = 0
+			B = 0
+			E = 0
+			V = random.randint(85, 101)
+
+			if (mov.categoria == FISICO):
+				A = self.ataque
+				D = Pokemon_rival.defensa
+			elif (mov.categoria == ESPECIAL):
+				A = self.ataque_esp
+				D = Pokemon_rival.defensa_esp
 
 			if (mov.tipo == self.tipo):
-				b = 1.5
+				B = 1.5
 			else:
-				b = 1
-			e = TablaEfectividad[mov.tipo][Pokemon_rival.tipo]
-			v = random.randint(85, 101) # Tiene que ser un valor discreto entre 85 y 100
+				B = 1
+			D = 300
+			if (mov.categoria != ESTADO):
+				E = TablaEfectividad[mov.tipo][Pokemon_rival.tipo]
 
-			daño = 0
-			if (mov.categoria == FISICO):
-				daño = int(0.01 * b * e * v * (((0.2 * self.nivel + 1) * self.ataque  * mov.potencia) / (25 * Pokemon_rival.defensa) + 2))
-			elif (mov.categoria == ESPECIAL):
-				daño = int(0.01 * b * e * v * (((0.2 * self.nivel + 1) * self.ataque_esp * mov.potencia) / (25 * Pokemon_rival.defensa_esp) + 2))
+			# Cálculo de daño
+			daño = int(0.01 * B * E * V * ((((0.2 * N + 1) * A * P)/ (25 * D)) + 2))
+			print ("N: "+str(N)+"\nA : "+str(A)+"\nP: "+str(P)+"\nD: "+str(D)+
+						 "\nB: "+str(B)+"\nE: "+str(E)+"\nV: "+str(V))
 
-			#	print(mov.nombre + " causó " + daño + " puntos de daño.")
+			# DEPURACIÓN
+			print(self)
+			print(Pokemon_rival)
+			print(mov.nombre + " causó " + str(daño) + " puntos de daño.")
+
 
 
 			# Aplicación del cálculo de daño
@@ -217,9 +296,11 @@ class Pokemon:
 			if (mov.nombre != 'combate'):
 				mov.pp -= 1
 
+		else: # El movimiento seleccionado no tiene PP suficientes
+			pass
 
 		# Devuelve el ataque utilizado por el pokémon
-		if (len(ataques_con_pp) == 0):
+		if (int_sel not in [0, 1, 2, 3]):
 			return "combate"
 		else:
 			return str(self.movimientos[int_sel].nombre).lower()
@@ -227,25 +308,21 @@ class Pokemon:
 # fed atacar
 
 
-	# El ataque se elige de forma automática
+	# Elige un ataque de forma automática y ataca, devuelve el ataque utilizado
 	def atacarIA (self, Pokemon_rival):
+		# Comprobar que el pokémon tenga PP en al menos un ataque
 		ataques_con_pp = [0, 1, 2, 3]
-		
-		# Comprobar ataques sin pp
 		for i in range(0, 4):
 			if (self.movimientos[i].pp == 0):
 				ataques_con_pp.remove(i)
 
 		# Si no hay pp se usará el ataque especial, "combate"
 		if len(ataques_con_pp) == 0:
-			return self.atacar(Pokemon_rival, 0)
+			return self.atacar(Pokemon_rival, -1)
 		else:
 			# Elegir uno de los ataques disponibles al azar
 			sel = ataques_con_pp[random.randint(0, len(ataques_con_pp))]
 			return self.atacar(Pokemon_rival, sel)
-
-		# Devuelve el ataque utilizado por el pokémon
-			
 
 
 	def restaurarPS (self, int_ps = 2000):
