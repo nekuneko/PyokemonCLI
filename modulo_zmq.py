5d137a2a39fb5cdbfc37892539279746bafe7720
nekuneko <noct@Melchior.local>
2017-05-26 04:04:17 +0200

    modulo preparado para establecer comunicacion con los modulos scrapy y twitter

--- modulo_zmq.py
+++ modulo_zmq.py
@@ -1,8 +1,59 @@
 import zmq
 import json
 
-contextTwitter = zmq.Context()
-contextDropbox = zmq.Context()
+# Las funciones init se auto inician, así que no es necesario llamarlas de forma explicita
+# pero en cambio SI ES NECESARIO CAMBIAR LA IP Y EL PUERTO AQUI SI FUERA NECESARIO
+GLOBAL_STR_IP 		= "10.182.106.194"
+GLOBAL_STR_PUERTO = "6969"
+
+def initTwitter (str_ip=GLOBAL_STR_IP, str_puerto=GLOBAL_STR_PUERTO):
+	global contextTwitter
+	global socketTwitter
+
+	contextTwitter 	= zmq.Context()
+	socketTwitter 	= contextTwitter.socket(zmq.PUSH)
+	socketTwitter.connect("tcp://"+str(str_ip)+':'+str(str_puerto))
+
+def initBusqueda (str_ip=GLOBAL_STR_IP, str_puerto=GLOBAL_STR_PUERTO):
+	global contextBusqueda
+	global socketBusqueda
+	contextBusqueda = zmq.Context()
+	socketBusqueda = contextBusqueda.socket(zmq.PUSH)	
+	socketBusqueda.connect("tcp://"+str(str_ip)+':'+str(str_puerto))
+
+
+
+# Publicar algo en twitter relacionado con un pokémon, primero debe de haberse ejecutado
+# al menos una vez initTwitter()
+def publicarTwitter (Pokemon_p, str_msg = "¡Pokémon salvaje apareció!", password = "pepe"):
+	initTwitter()
+	p_name 		= Pokemon_p.nombre
+	p_numero 	= Pokemon_p.numero
+	p_nivel 	= Pokemon_p.nivel
+	if (Pokemon_p.id == 0):
+		salvaje = True
+	else:
+		salvaje = False
+
+	# Si el pokémon es salvaje, mensaje que apareció,
+	# sino mensaje y punto
+
+	str_msg += '\n' + p_name + "   Nv: " + str(p_nivel)
+	str_json = {'password': password, 'mensaje': str_msg, 'salvaje': salvaje, 'numero': p_numero, 'nivel': p_nivel}
+	l_json = json.dumps(str_json)
+
+	socketTwitter.send_json(l_json)
+
+
+
+# Buscar Pokemon con scrapy en máquina remota, primero debe haberse ejecutado al menos
+# una vez initBusqueda()
+def buscarPokemonDbx (int_numero):
+	initBusqueda()
+	socketBusqueda.send_string(str(int_numero))
+
+
+
 
 # print("Conectando a twitter...")
 # socketTwitter = contextTwitter.socket(zmq.PUSH)
@@ -25,13 +76,13 @@ contextDropbox = zmq.Context()
 
 
 
-print("Conectando Fernando Server...")
-socketDropbox = contextDropbox.socket(zmq.PUSH)
-socketDropbox.connect("tcp://10.182.108.214:6969")
-print("Conectado a Fernando Server 2007 X Pro.")
+# print("Conectando Fernando Server...")
+# contextBusqueda = contextBusqueda.socket(zmq.PUSH)
+# contextBusqueda.connect("tcp://10.182.108.214:6969")
+# print("Conectado a Fernando Server 2007 X Pro.")
 
-for i in range(38, 50):
-	socketDropbox.send_string(str(i))
+# for i in range(38, 50):
+# 	contextBusqueda.send_string(str(i))
 
 
 
