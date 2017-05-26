c38c844990765479e1b09070a4a06fc6e4470475
nekuneko <noct@Melchior.local>
2017-05-26 04:02:04 +0200

    cambios menores para pruebas

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import menu
+import modulo_dropbox
+import pokemon
+from pokemon import *
+from pokemon_t import *
 
+# Iniciar el módulo dropbox
+# modulo_dropbox.initDropbox()
+# Consultar la lista del dropbox
+# l_pkm = modulo_dropbox.listarArchivos() 	
+# print(l_pkm)
+
+# Cargar base de datos de pokémon Local
+pokemon.cargarBaseDatosPkm()
+
+#pokemon.guardarBaseDatosPkm()
+
+
+p = Pokemon(36, -420)
+print(p)
+
+
+pokemon.cargarBaseDatosPkm()
 menu.next_move()
