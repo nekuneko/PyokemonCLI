0d5286b666697fb2fc52fe7713100e9a27657de0
nekuneko <noct@Melchior.local>
2017-05-27 15:19:30 +0200

    nada que destacar

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ import modulo_dropbox
 import pokemon
 from pokemon import *
 from pokemon_t import *
+import interfaz
 
 # Iniciar el módulo dropbox
-# modulo_dropbox.initDropbox()
+modulo_dropbox.initDropbox()
+
 # Consultar la lista del dropbox
 # l_pkm = modulo_dropbox.listarArchivos() 	
 # print(l_pkm)
 
 # Cargar base de datos de pokémon Local
 pokemon.cargarBaseDatosPkm()
-
-#pokemon.guardarBaseDatosPkm()
-
-
-p = Pokemon(36, -420)
-print(p)
-
-
-pokemon.cargarBaseDatosPkm()
 menu.next_move()
+
+# Guardar base de datos y salir
+pokemon.guardarBaseDatosPkm()
+print("Fin de la Partida")
+print("Gracias por jugar :D")
+
+
+
+
+
