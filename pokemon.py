e031914a540b05e848473d8b921b9ed113a30ad1
nekuneko <noct@Melchior.local>
2017-05-27 15:24:22 +0200

    cambios importantes
    añadidos nuevos pokemon a la base de datos local porque tienen
    multiples sprites en la 4a generacion y el scrapy se hace un lio.
    
    perfeccionada la forma en la que se accede a drobpox cuando se crea un
    pokemon
    
    modificada la función ataque para que devuelva un diccionario con
    multiple información sobre el mismo.
    
    modificadas las funciones de curación y añadida la función de grito del
    pokémon

--- pokemon.py
+++ pokemon.py
@@ -9,62 +9,137 @@ import time
 import os
 
 
-pokemon_legendarios = {'missingno.','moltres','zapdos','articuno','mewtwo','mew','raikou', 
-											 'entei','suicune','lugia','ho-oh','celebi','regirock','regice', 
-											 'registeel','latias','latios','kyogre','groudon','rayquaza','jirachi',
-											 'deoxys','uxie','mesprit','azelf','dialga','palkia','heatran','regigigas',
-											 'giratina','cresselia','phione','manaphy','darkrai','shaymin','arceus'}
+l_pokemonSinSexo = [
+	'baltoy','claydol','beldum','metang','metagross','ditto','voltorb','electrode',
+	'lunatone','solrock','magnemite','magneton','porygon','porygon2','starmie','staryu',
+	'unown']
 
-max_pokemon = 493 # solo pokemon del 0 al 493, 4ª generación
+
+l_pokemonLegendarios = [
+	'missingno.','moltres','zapdos','articuno','mewtwo','mew','raikou', 
+	'entei','suicune','lugia','ho-oh','celebi','regirock','regice', 
+	'registeel','latias','latios','kyogre','groudon','rayquaza','jirachi',
+	'deoxys','uxie','mesprit','azelf','dialga','palkia','heatran','regigigas',
+	'giratina','cresselia','phione','manaphy','darkrai','shaymin','arceus']
+
+l_pokemonSinSexo.extend(l_pokemonLegendarios)
+
+# Quedan excluido Deoxys por tener más de 1 sprite en pokemondb.net
+# El último pokémon representable es Deoxys
+max_pokemon = 386 # solo pokemon del 0 al 386, 3ª generación
 
 # Si el pokémon está en la base de datos significa que tenemos:
 # número, nombre, statsBase, foto delantera, foto trasera, y sonido
+# INCOMPLETO
+# AQUI DENTRO LOS MOVIMIENTOS ESTÁN PUESTOS COMO SETS, LUEGO HABRA QUE CAMBIARLOS
 pokemon_db = {
-	"0": {'nombre': "MissingNo.", 
-			'tipo': NORMAL,
-	 		'stats': {
-	 	 		'ps': 120, 'ataque': 120, 'defensa': 120, 
-	 	 		'ataque_esp': 120, 'defensa_esp': 120, 'velocidad': 120},
-	 	 	'movimientos': [
-	 	 		movimientoToSet(movimientos_db['']),
-	 	 		movimientoToSet(movimientos_db['']),
-	 	 		movimientoToSet(movimientos_db['']),
-	 	 		movimientoToSet(movimientos_db[''])]},
+	"0": {
+		'nombre': "MissingNo.", 
+		'tipo': NORMAL,
+ 		'stats': {
+ 	 		'ps': 120, 'ataque': 120, 'defensa': 120, 
+ 	 		'ataque_esp': 120, 'defensa_esp': 120, 'velocidad': 120},
+ 	 	'movimientos': [
+ 	 		movimientoToSet(movimientos_db['']),
+ 	 		movimientoToSet(movimientos_db['']),
+ 	 		movimientoToSet(movimientos_db['']),
+ 	 		movimientoToSet(movimientos_db[''])]},
 
- 	"1": {'nombre': "Bulbasaur", 
-			'tipo': PLANTA,
-	 		'stats': {
-	 	 		'ps': 45, 'ataque': 49, 'defensa': 49, 
-	 	 		'ataque_esp': 65, 'defensa_esp': 65, 'velocidad': 45},
-	 	 	'movimientos': [
-	 	 		movimientoToSet(movimientos_db['placaje']),
-	 	 		movimientoToSet(movimientos_db['latigo cepa']),
-	 	 		movimientoToSet(movimientos_db['hoja afilada']),
-	 	 		movimientoToSet(movimientos_db[''])]},
+ 	"1": {
+ 		'nombre': "Bulbasaur", 
+		'tipo': PLANTA,
+ 		'stats': {
+ 	 		'ps': 45, 'ataque': 49, 'defensa': 49, 
+ 	 		'ataque_esp': 65, 'defensa_esp': 65, 'velocidad': 45},
+ 	 	'movimientos': [
+ 	 		movimientoToSet(movimientos_db['placaje']),
+ 	 		movimientoToSet(movimientos_db['latigo cepa']),
+ 	 		movimientoToSet(movimientos_db['hoja afilada']),
+ 	 		movimientoToSet(movimientos_db[''])]},
  	
- 	# AQUI DENTRO LOS MOVIMIENTOS ESTÁN PUESTOS COMO SETS, LUEGO HABRA QUE CAMBIARLOS
- 	"382": {'nombre': "Kyogre",
- 				'tipo': AGUA,
- 				'stats': {
-	 	 			'ps': 100, 'ataque': 100, 'defensa': 90, 
-	 	 			'ataque_esp': 150, 'defensa_esp': 140, 'velocidad': 90},
-	 	 		'movimientos': [
-		 	 		movimientoToSet(movimientos_db['ventisca']),
-		 	 		movimientoToSet(movimientos_db['surf']),
-		 	 		movimientoToSet(movimientos_db['salpicar']),
-		 	 		movimientoToSet(movimientos_db['hidrobomba'])]},
+ 	"25": {
+ 		'nombre': "Pikachu",
+ 		'tipo': ELECTRICO,
+			'stats': {
+	 			'ps': 35, 'ataque': 55, 'defensa': 40, 
+	 			'ataque_esp': 50, 'defensa_esp': 50, 'velocidad': 90},
+	 		'movimientos': [
+ 	 		movimientoToSet(movimientos_db['impactrueno']),
+ 	 		movimientoToSet(movimientos_db['ataque rapido']),
+ 	 		movimientoToSet(movimientos_db['rayo']),
+ 	 		movimientoToSet(movimientos_db['chispa'])]},
+ 
+ 	"201": {
+ 		'nombre': "Unown",
+		'tipo': PSIQUICO,
+		'stats': {
+ 			'ps': 48, 'ataque': 72, 'defensa': 48, 
+ 			'ataque_esp': 72, 'defensa_esp': 48, 'velocidad': 48},
+ 		'movimientos': [
+	 		movimientoToSet(movimientos_db['poder oculto']),
+	 		movimientoToSet(movimientos_db['']),
+	 		movimientoToSet(movimientos_db['']),
+	 		movimientoToSet(movimientos_db[''])]},
+ 
+	"327": {
+		'nombre': "Spinda",
+		'tipo': NORMAL,
+		'stats': {
+ 			'ps': 60, 'ataque': 60, 'defensa': 60, 
+ 			'ataque_esp': 60, 'defensa_esp': 60, 'velocidad': 60},
+ 		'movimientos': [
+	 		movimientoToSet(movimientos_db['placaje']),
+	 		movimientoToSet(movimientos_db['alboroto']),
+	 		movimientoToSet(movimientos_db['psicorrayo']),
+	 		movimientoToSet(movimientos_db['finta'])]},
+
+ "351": {
+ 	'nombre': "Castform",
+	'tipo': NORMAL,
+	'stats': {
+			'ps': 70, 'ataque': 70, 'defensa': 70, 
+			'ataque_esp': 70, 'defensa_esp': 70, 'velocidad': 70},
+		'movimientos': [
+ 		movimientoToSet(movimientos_db['placaje']),
+ 		movimientoToSet(movimientos_db['pistola agua']),
+ 		movimientoToSet(movimientos_db['ascuas']),
+ 		movimientoToSet(movimientos_db['golpe cabeza'])]},
+
+	"382": {
+		'nombre': "Kyogre",
+		'tipo': AGUA,
+		'stats': {
+ 			'ps': 100, 'ataque': 100, 'defensa': 90, 
+ 			'ataque_esp': 150, 'defensa_esp': 140, 'velocidad': 90},
+ 		'movimientos': [
+	 		movimientoToSet(movimientos_db['ventisca']),
+	 		movimientoToSet(movimientos_db['surf']),
+	 		movimientoToSet(movimientos_db['salpicar']),
+	 		movimientoToSet(movimientos_db['hidrobomba'])]},
 	 	 				 
- 	
- 	"383": {'nombre': "Groudon",
- 				'tipo': TIERRA,
- 				'stats': {
-	 	 			'ps': 100, 'ataque': 150, 'defensa': 140, 
-	 	 			'ataque_esp': 100, 'defensa_esp': 90, 'velocidad': 90},
-	 	 		'movimientos': [
-		 	 		movimientoToSet(movimientos_db['llamarada']),
-		 	 		movimientoToSet(movimientos_db['rayo solar']),
-		 	 		movimientoToSet(movimientos_db['terremoto']),
-		 	 		movimientoToSet(movimientos_db['estallido'])]}				
+ 	"383": {
+ 		'nombre': "Groudon",
+		'tipo': TIERRA,
+		'stats': {
+ 			'ps': 100, 'ataque': 150, 'defensa': 140, 
+ 			'ataque_esp': 100, 'defensa_esp': 90, 'velocidad': 90},
+ 		'movimientos': [
+	 		movimientoToSet(movimientos_db['llamarada']),
+	 		movimientoToSet(movimientos_db['rayo solar']),
+	 		movimientoToSet(movimientos_db['terremoto']),
+	 		movimientoToSet(movimientos_db['estallido'])]},	
+
+	"386": {
+ 		'nombre': "Deoxys",
+		'tipo': PSIQUICO,
+		'stats': {
+ 			'ps': 50, 'ataque': 150, 'defensa': 50, 
+ 			'ataque_esp': 150, 'defensa_esp': 50, 'velocidad': 150},
+ 		'movimientos': [
+	 		movimientoToSet(movimientos_db['desarme']),
+	 		movimientoToSet(movimientos_db['psiquico']),
+	 		movimientoToSet(movimientos_db['psicoataque']),
+	 		movimientoToSet(movimientos_db['hiperrayo'])]}					
 }
 
 
@@ -73,9 +148,24 @@ def cargarBaseDatosPkm ():
 	try:
 		with open("pokemon.db") as json_file:
 			pokemon_db = json.load(json_file)
+			
+			# Añadir a missigno, obligatorio
+			pokemon_db["0"] = {
+				'nombre': "MissingNo.", 
+				'tipo': NORMAL,
+ 				'stats': {
+ 	 			'ps': 120, 'ataque': 120, 'defensa': 120, 
+ 	 			'ataque_esp': 120, 'defensa_esp': 120, 'velocidad': 120},
+ 	 			'movimientos': [
+ 	 				movimientoToSet(movimientos_db['']),
+ 	 				movimientoToSet(movimientos_db['']),
+ 	 				movimientoToSet(movimientos_db['']),
+ 	 				movimientoToSet(movimientos_db[''])]}
+
 	except Exception as e:
 		# No se encuentra el archivo, usar la base de datos estática del programa
 		guardarBaseDatosPkm()
+
 	
 
 def guardarBaseDatosPkm ():
@@ -207,11 +297,11 @@ class Pokemon:
 		int_nivel  = int(int_nivel)
 		int_id_entrenador = int(int_id_entrenador)
 
-		# Establacer número de pokémon
+		# Establacer número de pokémon, si está fuera del rango representable será missigno
 		if (int_numero < 0):
 			self.numero = 0
 		elif (int_numero > max_pokemon):
-			self.numero = max_pokemon
+			self.numero = 0
 		else:
 			self.numero = int_numero
 
@@ -231,9 +321,6 @@ class Pokemon:
 
 		# El sexo se establece al final
 
-		# Poner movimientos por defecto o de base de datos
-		self.setMovimientos(l4_movimientos)
-
 		# Buscar pokemon por número en la base de datos local, 
 		# si está en la base de datos local tenemos las imágenes y todo
 		if (str(self.numero) in pokemon_db):
@@ -244,7 +331,8 @@ class Pokemon:
 			# Poner los stats base de la base de datos
 			self.setStatsBase(self.numero, self.nivel)
 
-			#self.setMovimientos(l4_movimientos)
+			# Poner movimientos de la base de datos o introducidos
+			self.setMovimientos(l4_movimientos)
 			# sino se preservarán los movimientos introducidos
 					
 
@@ -263,20 +351,18 @@ class Pokemon:
 				# Trato de buscar el pokémon en dropbox durante 1 minuto (5 segundos * 12)
 				bool_pokemonEnDropbox = False
 				for i in range (0, 12):
-					#print(i)
-					modulo_zmq.buscarPokemonDbx(self.numero) 	# mando petición de busqueda
-					time.sleep(5) 														# espero 5 segundos
-					l_pkm = modulo_dropbox.listarArchivos() 	# vuelvo a consultar la lista
-					if (str(self.numero) + ".jpg" in l_pkm): 	# si el pokémon está en dropbox, True
+					l_pkm = modulo_dropbox.listarArchivos() 	# Consulto la lista
+					if (str(self.numero) + ".png" in l_pkm): 	# si el pokémon está en dropbox, True
 						bool_pokemonEnDropbox = True
-						break 																	# y me salgo del bucle
-
+						break																		# y me salgo del bucle
+					else:							
+						modulo_zmq.buscarPokemonDbx(self.numero) 	# mando petición de busqueda
+						time.sleep(5) 																	# espero 5 segundos
+					
 				# El pokémon está en dropbox
 				if (bool_pokemonEnDropbox): 
 					# Me descargo los archivos en el directorio, por defecto img/
 					modulo_dropbox.descargarArchivos(self.numero)
-
-					#print("El pokemon está en dropbox")
 
 					# Coger datos de json y añadirlos a base de datos
 					set_p = jsonDropboxToSet(self.numero)
@@ -295,13 +381,22 @@ class Pokemon:
 					for i in range(0, len(set_p['movimientos'])):
 						l4_movimientos.append(setToMovimiento(set_p['movimientos'][i]))
 
+					# print("Movimietnos. Antes")
+					# for i in range(0, len(l4_movimientos)):
+					# 	print(l4_movimientos[i])
+
+
 					# Asignar movimientos al pokemon
 					self.setMovimientos(l4_movimientos)
 
+					# print("Movimietnos. Despues")
+					# for i in range(0, len(self.movimientos)):
+					# 	print(self.movimientos[i])
 
-				# Ha pasado más de 1 minuto y el pokémon no se encuentra en dropbox
+
+				# Ha pasado más de 1 minuto y el pokémon no se encuentra en dropbox,
+				# por lo tanto, el pokémon es MissingNo. 
 				else:
-					# Todo ha fallado, el pokemon es MissingNo.
 					self.MissingNo()
 
 			# No usamos dropbox y el pokémon no está en la base de datos, 
@@ -312,7 +407,7 @@ class Pokemon:
 
 
 		# Determinar sexo del pokémon
-		if (self.nombre.lower() in pokemon_legendarios): # Si Pkm legendario, no tiene sexo
+		if (self.nombre.lower() in l_pokemonSinSexo): # Si pokemon sin sexo, no tiene genero
 			self.sexo = NOGENERO
 		elif (t_sexo == NOESPECIFICADO): # Si no se especificó y no es legendario se genera al azar
 			self.sexo = random.randint(0, 2)
@@ -328,12 +423,13 @@ class Pokemon:
 		# Si la lista es de menos de 4 elementos, rellenar con "movimiento nulo"
 		if (len(self.movimientos) < 4):
 			for i in range(len(self.movimientos), 4):
-				self.movimientos.append(movimientoToSet(movimientos_db['']))
+				self.movimientos.append(movimientos_db[''])
 		elif (len(self.movimientos) > 4): 
 			while (len(self.movimientos) > 4): # Si tiene más de 4 elementos vamos quitando
 				self.movimientos.pop()	         # los que sobren
 
-		# Si el usuario no introdujo movimientos, tirar de base de datos movimientos
+		# Si el usuario no introdujo movimientos, 
+		# tirar de los movimientos de la base de datos de pokemon
 		if (len(l4_movimientos) == 0):
 			try:
 				for i in range(0, 4):
@@ -374,6 +470,8 @@ class Pokemon:
 		for i in range(0, 4):
 			self.movimientos.append(setToMovimiento(pokemon_db["0"]['movimientos'][i]))
 		
+
+
 
 
 	# Desde pokemon_db o a pelo
@@ -456,16 +554,23 @@ class Pokemon:
 			self.velocidad 		+= 1
 
 
+	# Devuelve un diccionario con la siguiente informacion:
+	# Nombre del ataque realizado, efectividad sobre el oponente, y si ha acertado o no.
+	# {'nombre': "Combate", 'efectividad': int_efectividad, 'acierto': bool_haAcertado}
 	def atacar (self, Pokemon_rival, int_sel):
+		int_sel = int(int_sel)
+
 		# Si ningún ataque del pokémon tiene PP, este usará combate
 		if (int_sel not in [0, 1, 2, 3]): 
 			mov = movimientos_db['combate']
 		else:
 			mov = self.movimientos[int_sel] # movimiento seleccionado
 
-		
-		# Si el movimiento tiene PP suficientes, ataca
-		if (mov.pp > 0):
+
+		bool_haAcertado = False
+		float_efectividad = -1
+		# Si el movimiento tiene PP suficientes y no es un ataque de categoria ESTADO, ataca
+		if (mov.pp > 0 and mov.categoria != ESTADO):
 			# CÁLCULO DE DAÑO
 			#
 			# DAÑO = 0.01 * B * E * V * ((((0.2 * N + 1) * A * P)/ (25 * D)) + 2) 
@@ -504,8 +609,8 @@ class Pokemon:
 			else:
 				B = 1
 			D = 300
-			if (mov.categoria != ESTADO):
-				E = TablaEfectividad[mov.tipo][Pokemon_rival.tipo]
+
+			E = TablaEfectividad[int(mov.tipo)][int(Pokemon_rival.tipo)]
 
 			# Cálculo de daño
 			daño = int(0.01 * B * E * V * ((((0.2 * N + 1) * A * P)/ (25 * D)) + 2))
@@ -526,20 +631,26 @@ class Pokemon:
 			else:
 				Pokemon_rival.ps -= daño
 
+
+			if (daño > 0):
+				bool_haAcertado = True
+				float_efectividad = float(E)
+
 			# Si el ataque no es combate, disminuyen los pp del ataque
 			if (mov.nombre != 'combate'):
 				mov.pp -= 1
 
-		else: # El movimiento seleccionado no tiene PP suficientes
+		else: # El movimiento seleccionado no tiene PP suficientes o es de categoria ESTADO
 			pass
 
-		# Devuelve el ataque utilizado por el pokémon
+		# Devuelve el ataque utilizado por el pokémon, util para el ataque de IA
 		if (int_sel not in [0, 1, 2, 3]):
-			return "combate"
+			return {'ataque': "Combate", 'efectividad': float_efectividad, 'acierto': bool_haAcertado}
 		else:
-			return str(self.movimientos[int_sel].nombre).lower()
+			str_nombre = str(self.movimientos[int_sel].nombre).title()
+			return {'ataque': str_nombre, 'efectividad': float_efectividad, 'acierto': bool_haAcertado}
 
-# fed atacar
+# fin atacar
 
 
 	# Elige un ataque de forma automática y ataca, devuelve el ataque utilizado
@@ -547,7 +658,7 @@ class Pokemon:
 		# Comprobar que el pokémon tenga PP en al menos un ataque
 		ataques_con_pp = [0, 1, 2, 3]
 		for i in range(0, 4):
-			if (self.movimientos[i].pp == 0):
+			if (self.movimientos[i].pp <= 0):
 				ataques_con_pp.remove(i)
 
 		# Si no hay pp se usará el ataque especial, "combate"
@@ -559,18 +670,34 @@ class Pokemon:
 			return self.atacar(Pokemon_rival, sel)
 
 
-	def restaurarPS (self, int_ps = 2000):
-		if ((self.ps + int_ps) > self.ps_max):
+
+
+
+	def gritar(self):
+		os.system("afplay sounds/"+str(self.numero)+".wav &");
+
+
+	def restaurarPP (self, int_mov, int_pp = -1):
+		int_pp = int(int_pp)
+
+		# Si int_pp es negativo o la suma con los pp es mayor que los pp maximos, curar todos
+		if ((int_pp < 0) or 
+			 	((self.movimientos[int_mov].pp + int_pp) > self.movimientos[int_mov].pp_max)):
+			self.movimientos[int_mov].pp = self.movimientos[int_mov].pp_max
+		else:
+			self.movimientos[int_mov].pp += int_pp
+
+
+	def restaurarPS (self, int_ps = -1):
+		int_ps = int(int_ps)
+
+		# Si int_ps es negativo o la suma con los ps es mayor que los ps maximos, curar todos
+		if ((int_ps < 0) or ((self.ps + int_ps) > self.ps_max)):
+			# restaurar todos los ps
 			self.ps = self.ps_max
 		else:
 			self.ps += int_ps
 
-
-	def restaurarPP (self, int_mov, int_pp = 100):
-		if ((self.movimientos[int_mov].pp + int_pp) > self.movimientos[int_mov].pp_max):
-			self.movimientos[int_mov].pp = self.movimientos[int_mov].pp_max
-		else:
-			self.movimientos[int_mov].pp += int_pp
 
 	def restauraTodo (self):
 		self.restaurarPS();
@@ -578,6 +705,8 @@ class Pokemon:
 			self.restaurarPP(i)
 
 
+	# Esta es la foma abreviada de imprimir datos de un pokemon, sirve para imprimir
+	# los pokémon del equipo principalmente y su estado
 	def list (self, l):
 		b_max = l-2
 		b = int(self.ps * b_max / self.ps_max)
@@ -593,7 +722,7 @@ class Pokemon:
 						"Nivel: " + str(self.nivel),
 						"Tipo: " + dic_tipo[self.tipo],
 					  "("+str_vida+")",
-						"\tPs: " + str(self.ps) + '/' + str(self.ps_max),
+						"\tPS: " + str(self.ps) + '/' + str(self.ps_max),
 						"-"*l]
 
 
